484ff0d9f782174fc58337b314c3f569c3a7e5bd
kazurayam <kazuaki.urayama@gmail.com>
2022-02-04 09:57:49 +0900

    add startup-flaskr.sh

---
+++ docs/UI_test_webdriver_docker.md
@@ -0,0 +1,6 @@
+<!-- START doctoc -->
+<!-- END doctoc -->
+
+# Automated UI Test using WebDriver backed by Docker Container
+
+TODO
---
+++ startup-flaskr.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+CWD=$(pwd)
+cd $(mktemp -d)
+
+echo you can visit http://127.0.0.1/
+
+docker run -it -p 80:8080 kazurayam/flaskr-kazurayam:1.1.0
+
+cd $CWD
