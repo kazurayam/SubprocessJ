d8d859a76a2681db0eb49ec83a9fa142361ef13f
kazurayam <kazuaki.urayama@gmail.com>
2022-02-05 09:22:11 +0900

    #9 tested DockerCommandLocator on Windows10

--- src/test/java/com/kazurayam/subprocessj/docker/DockerCommandLocatorTest.java
+++ src/test/java/com/kazurayam/subprocessj/docker/DockerCommandLocatorTest.java
@@ -11,12 +11,12 @@ public class DockerCommandLocatorTest {
     @Test
     void test_smoke() {
         CommandLocatingResult clr = DockerCommandLocator.find();
-        assertEquals(0, clr.returncode());
+        assertEquals(0, clr.returncode(), "You do not have docker installed.\n" + clr.toString());
         String path = clr.stdout().get(0).trim();
         if (OSType.isMac() || OSType.isUnix()) {
             assertEquals("/usr/local/bin/docker", path);
         } else if (OSType.isWindows()) {
-            assertEquals("C:\\Program File\\docke\\docker.exe", path);
+            assertEquals("C:\\Program Files\\Docker\\Docker\\resources\\bin\\docker", path);
         }
 
     }
